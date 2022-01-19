a09d8213473c4eb0984cfa05c24973d558a95c8a
minsik7180 <la9814@naver.com>
2022-01-19 20:44:09 +0900

    알고리즘

---
+++ eclipse-workspace/알고리즘실습/src/스택/IntStack.java
@@ -0,0 +1,82 @@
+package 스택;
+
+public class IntStack {
+	private int max; // 스택 용량 
+	private int ptr; // 스택 포인터 
+	private int[] stk; // 스택 본체 
+	
+	
+	//스택이 비어있음 
+	public class EmptyIntStackExeption extends RuntimeException{
+		public EmptyIntStackExeption() { }
+	}
+	//스택이 가득 참 
+	public class OverflowIntStackException extends RuntimeException{
+		public OverflowIntStackException() { }
+	}
+	
+	public IntStack(int capacity) {
+		ptr = 0;
+		max = capacity;
+		try {
+			stk=new int[max];   //스택 본체 배열 생성 
+		}catch (OutOfMemoryError e) {   //생성할 수 없음 
+			max = 0;
+		}
+	}
+	
+	public int push(int x) throws OverflowIntStackException{
+		if (ptr >= max)
+			throw new OverflowIntStackException();
+		return stk[ptr++] = x;
+	}
+	
+	public int pop() throws EmptyIntStackExeption{
+		if(ptr <= 0)
+			throw new EmptyIntStackExeption();
+		
+		return stk[--ptr];
+	}
+	
+	public int peek() throws EmptyIntStackExeption{
+		if(ptr <= 0)
+			throw new EmptyIntStackExeption();
+		return stk[ptr -1];
+	}
+	public int indexOf(int x) {
+		for(int i= ptr -1; i>=0; i--)
+			if(stk[i] == x)
+				return i;
+		return -1;
+	}
+	
+	public void clear() {
+		ptr = 0;
+	}
+	
+	public int capacity() {
+		return max;
+	}
+	
+	public int size() {
+		return ptr;
+	}
+	
+	public boolean isEmpty() {
+		return ptr<=0;
+	}
+	
+	public boolean isFull() {
+		return ptr >=max;
+	}
+	
+	public void dump() {
+		if(ptr <=0)
+			System.out.println("스택이 비어있습니다 ");
+		else {
+			for(int i=0; i<ptr; i++) 
+				System.out.print(stk[i] + " ");
+			System.out.println();
+		}
+	}
+}
---
+++ eclipse-workspace/알고리즘실습/src/스택/IntStackTester.java
@@ -0,0 +1,87 @@
+package 스택;
+
+import java.util.Scanner;
+
+import 스택.IntStack.EmptyIntStackExeption;
+
+public class IntStackTester {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		IntStack s = new IntStack(64);
+		
+		while(true) {
+			System.out.println("현재 데이터 수 : " + s.size() + " / "+ s.capacity());
+			
+			System.out.print("(1) 푸시 (2) 팝 (3) 피크 (4) 덤프 (5) 크기 (6) 상태 (7) 스택 용량 (8) 초기화 (0) 종료 : ");
+			
+			int menu = stdIn.nextInt();
+			
+			if(menu == 0 )break;
+			
+			int x;
+			boolean t , n;
+			switch(menu) { 
+				case 1:     // 푸시
+					System.out.print("데이터 : ");
+					x = stdIn.nextInt();
+					try {
+						s.push(x);
+					}catch (IntStack.OverflowIntStackException e) {
+						System.out.println("스택이 가득 찼습니다");
+					}
+					break;
+				
+				case 2:  // 팝
+					try {
+						x=s.pop();
+						System.out.println("팝한 데이터는 " + x + "입니다");
+					}catch (IntStack.EmptyIntStackExeption e) {
+						System.out.println("스택이 비어있습니다 ");
+					}
+					break;
+					
+				case 3:  // 피크
+					try {
+						x = s.peek();
+						System.out.println("피크 한 데이터는 " + x + "입니다");
+					}catch(IntStack.EmptyIntStackExeption e) {
+						System.out.println("스택이 비어있습니다");
+					}
+					break;
+					
+				case 4:   // 덤프
+					s.dump();
+					break;
+				
+				case 5:
+					x= s.size();
+					System.out.println("스택의 크기는 " + x + "입니다");
+					break;
+					
+				case 6:
+					t = s.isEmpty();
+					n = s.isFull();
+					if(t == true) {
+						System.out.println("스택이 비어있습니다");
+					}
+					else if(n == true) {
+						System.out.println("스택이 가득 찼습니다");
+					}
+					else {
+						System.out.println("스택에 요소가 있고 공간이 있습니다");
+					}
+					break;
+				
+				case 7:
+					x=s.capacity();
+					System.out.println("스택의 최대 용량은 " + x + "입니다");
+					break;
+				case 8:
+					s.clear();
+					System.out.println("스택이 초기화 되었습니다");
+			}
+		}
+	}
+
+}
